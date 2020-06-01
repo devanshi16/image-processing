6ebde3a9593271ea534831b7fdb9b22ed9b777aa
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-06-01 16:00:38 -0400

    filter

--- grayscale.py
+++ grayscale.py
@@ -2,21 +2,15 @@ import image
 img = image.Image('Tele.gif')
 win = image.ImageWin(img.getWidth(), img.getHeight())
 img.draw(win)
-img.setDelay(1,1000)   # setDelay(1, 2000) will speed up a lot                      # img.setDelay(delay, number of pixels between delay)
+img.setDelay(1,1000)   # setDelay(1, 2000) will speed up a lot   
 
 for row in range(img.getHeight()):
     for col in range(img.getWidth()):
         p = img.getPixel(col, row)
 
         gray_value = int((p.getRed() + p.getGreen() + p.getBlue())/3)
-
-        newred = gray_value
-        newgreen = gray_value
-        newblue =  gray_value
-
-        newpixel = image.Pixel(newred, newgreen, newblue)
+        newpixel = image.Pixel(gray_value, gray_value, gray_value)
 
         img.setPixel(col, row, newpixel)
-
 img.draw(win)
 win.exitonclick()
